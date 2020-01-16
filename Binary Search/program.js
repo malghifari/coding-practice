91a97bb5797d654760b312f9847587fa781ac93a
Mochamad Alghifari <mo.alghifari@gmail.com>
2020-01-16 14:33:48 +0700

    Add recursive binary search solution

--- Binary Search/program.js
+++ Binary Search/program.js
@@ -1,14 +1,10 @@
 function binarySearch(array, target) {
 	let left = 0;
 	let right = array.length - 1;
-	while (right > left) {
+	while (right >= left) {
 		const pivot = Math.floor((right + left) / 2);
 		if (array[pivot] === target) {
 			return pivot;
-		} else if (array[left] === target) {
-			return left;
-		} else if (array[right] === target) {
-			return right;
 		} else if (array[pivot] < target) {
 			left = pivot + 1;
 		} else {
@@ -16,6 +12,18 @@ function binarySearch(array, target) {
 		}
 	}
 	return -1;
+}
+
+function binarySearch(array, target) {
+	return binarySearchHelper(array, target, 0, array.length - 1)
+}
+
+function binarySearchHelper(array, target, left, right) {
+    if (right < left) return -1;
+    const pivot = Math.floor((right + left) / 2);
+    if (array[pivot] === target) return pivot;
+    else if (array[pivot] < target) return binarySearchHelper(array, target, pivot + 1, right);
+    else if (array[pivot] > target) return binarySearchHelper(array, target, left, right - 1);
 }
 
 // Do not edit the line below.
